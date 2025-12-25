4ce9594ef10d06375212aa9f21a0d868b1fdfd68
Javier Tamayo <javierj.tamayo@gmail.com>
2025-12-24 23:43:55 -0500

    Init Setup

---
+++ server/src/index.ts
@@ -0,0 +1,13 @@
+import express, { type Request, type Response } from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send(`Server Successfully Running on PORT: ${PORT}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server Successfully Running on PORT: ${PORT}`);
+});
